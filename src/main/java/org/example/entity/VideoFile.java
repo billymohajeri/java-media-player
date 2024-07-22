765e499fc8ad1f4fdedbd3a723d7f90c3b4f25fd
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-07-22 20:17:51 +0300

    Add videoFile class

---
+++ src/main/java/org/example/entity/VideoFile.java
@@ -0,0 +1,20 @@
+package org.example.entity;
+
+public class VideoFile extends MediaFile {
+  int volume;
+  int brightness;
+
+  public VideoFile(int id, String name, String type, int volume, int brightness) {
+    super(id, name, type);
+    this.volume = volume;
+    this.brightness = brightness;
+  }
+
+  public void adjustVolume(int volume) {
+
+  }
+
+  public void adjustBrightness(int brightness) {
+
+  }
+}
