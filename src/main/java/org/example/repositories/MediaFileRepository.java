e6c94765b3f32a3e2614606bec0379288bd73e3c
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-07-22 21:46:59 +0300

    Add mediaFileRepository & playlistRepository class

---
+++ src/main/java/org/example/repositories/MediaFileRepository.java
@@ -0,0 +1,25 @@
+package org.example.repositories;
+
+import org.example.entities.MediaFile;
+
+public class MediaFileRepository {
+  public void addMediaFile(MediaFile mediaFile) {
+
+  }
+
+  public void removeMediaFile(MediaFile mediaFile) {
+
+  }
+
+  public void updateMediaFile(MediaFile mediaFile) {
+
+  }
+
+  public void getMediaFileById(int id) {
+
+  }
+
+  public void getAllMediaFiles() {
+
+  }
+}
---
+++ src/main/java/org/example/repositories/PlaylistRepository.java
@@ -0,0 +1,24 @@
+package org.example.repositories;
+
+public class PlaylistRepository {
+
+  public void addPlaylist(Playlist playlist) {
+
+  }
+
+  public void removePlaylist(Playlist playlist) {
+
+  }
+
+  public void updatePlaylist(Playlist playlist) {
+
+  }
+
+  public void getPlaylistById(int id) {
+
+  }
+
+  public void getAllPlaylists() {
+
+  }
+}
