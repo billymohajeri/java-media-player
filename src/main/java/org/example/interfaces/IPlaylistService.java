309bb064bb4f47b8d2b7685cda40161b387ebeca
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-07-22 21:54:39 +0300

    Add IPlaylistService class

---
+++ src/main/java/org/example/interfaces/IPlaylistService.java
@@ -0,0 +1,30 @@
+package org.example.interfaces;
+
+import org.example.entities.Playlist;
+
+public class IPlaylistService {
+
+  public void createPlaylist(Playlist playlist) {
+  }
+
+  public void deletePlaylist(int id) {
+  }
+
+  public void updatePlaylist(Playlist playlist) {
+  }
+
+  public void getPlaylist(int id) {
+  }
+
+  public void listPlaylists() {
+  }
+
+  public void playMediaFromPlaylist(int playlistId, int mediaFileId) {
+  }
+
+  public void pauseMediaInPlaylist(int playlistId, int mediaFileId) {
+  }
+
+  public void stopMediaInPlaylist(int playlistId, int mediaFileId) {
+  }
+}
