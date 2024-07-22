2d548d4507e3cd2f9688a0009a4e9769d7bd656c
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-07-22 21:53:03 +0300

    Add IMediaFileService class

---
+++ src/main/java/org/example/interfaces/IMediaFileService.java
@@ -0,0 +1,38 @@
+package org.example.interfaces;
+
+import org.example.entities.MediaFile;
+
+public class IMediaFileService {
+  public void addMediaFile(MediaFile mediaFile) {
+  }
+
+  public void deleteMediaFile(int id) {
+  }
+
+  public void updateMediaFile(MediaFile mediaFile) {
+  }
+
+  public void getMediaFile(int id) {
+  }
+
+  public void listMediaFiles() {
+  }
+
+  public void playMedia(int id) {
+  }
+
+  public void pauseMedia(int id) {
+  }
+
+  public void stopMedia(int id) {
+  }
+
+  public void adjustVolume(int id, int volume) {
+  }
+
+  public void adjustBrightness(int id, int brightness) {
+  }
+
+  public void changeSoundEffect(int id, String effect) {
+  }
+}
