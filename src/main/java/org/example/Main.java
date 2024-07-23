dcd15977d19ffd83a4720276ec3ca41f47452b74
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-07-23 20:41:08 +0300

    Update main to show get media file by id

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ import org.example.repositories.MediaFileRepository;
 import org.example.services.MediaFileService;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class Main {
@@ -23,7 +24,7 @@ public class Main {
     String choice = "";
     while (!choice.equalsIgnoreCase("Q")) {
       System.out.println(BLUE + "\n1- Get all media files");
-      System.out.println("2- User");
+      System.out.println("2- Get media file by ID");
       System.out.println("3- Play List");
       System.out.println("Q- Quit");
       System.out.println(BLUE + "Enter your choice: ");
@@ -34,7 +35,23 @@ public class Main {
             System.out.println(YELLOW + mediaFile.getId() + ": " + mediaFile.getName() + " - " + mediaFile.getType());
           }
         }
-        case "2" -> System.out.println("2");
+        case "2" -> {
+          System.out.print("Enter the ID: ");
+          String id = scanner.nextLine();
+          try {
+            Optional<MediaFile> optionalMediaFile = mediaFileService.getMediaFile(Integer.parseInt(id));
+            if (optionalMediaFile.isPresent()) {
+              MediaFile mediaFile = optionalMediaFile.get();
+              System.out.println(YELLOW + id + ": " + mediaFile.getName() + " - " + mediaFile.getType());
+            } else {
+              System.out.println(RED + "Media file with ID " + id + " not found.");
+            }
+          } catch (NumberFormatException e) {
+            System.out.println(RED + "Invalid ID format: " + id);
+          } catch (Exception e) {
+            System.out.println(RED + "Error: " + e.getMessage());
+          }
+        }
         case "3" -> System.out.println("3");
         case "Q" -> System.out.println(RED + "Exiting the app...");
         default -> System.out.println(RED + "Invalid choice, try again!");
