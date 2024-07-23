7daec52ae2baa907f00abc7f6a6dc9f88ec028ab
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-07-24 00:07:43 +0300

    Add methods to print users & media files by id to main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,20 +16,16 @@ public class Main {
   private static final String RED = "\033[0;31m";
   private static final String BLUE = "\033[0;34m";
   private static final String YELLOW = "\033[0;33m";
+  private static final Database database = new Database();
+  private static final MediaFileRepository mediaFileRepository = new MediaFileRepository(database);
+  private static final MediaFileService mediaFileService = new MediaFileService(mediaFileRepository);
+  private static final List<MediaFile> mediaFiles = mediaFileService.listMediaFiles();
+  private static final UserRepository userRepository = new UserRepository(database);
+  private static final UserService userService = new UserService(userRepository);
+  private static final List<User> users = userService.listUsers();
+  private static Scanner scanner = new Scanner(System.in);
 
   public static void main(String[] args) {
-
-    Database database = new Database();
-
-    MediaFileRepository mediaFileRepository = new MediaFileRepository(database);
-    MediaFileService mediaFileService = new MediaFileService(mediaFileRepository);
-    List<MediaFile> mediaFiles = mediaFileService.listMediaFiles();
-
-    UserRepository userRepository = new UserRepository(database);
-    UserService userService = new UserService(userRepository);
-    List<User> users = userService.listUsers();
-
-    Scanner scanner = new Scanner(System.in);
     String choice = "";
     while (!choice.equalsIgnoreCase("Q")) {
       System.out.println(BLUE + "\n1- Get all media files");
@@ -39,48 +35,68 @@ public class Main {
       System.out.println("Q- Quit");
       System.out.println(BLUE + "Enter your choice: ");
       choice = scanner.nextLine().toUpperCase();
+
       switch (choice) {
-        case "1" -> printAllMediaFiles(mediaFiles);
-        case "2" -> {
-          System.out.print("Enter the ID: ");
-          String id = scanner.nextLine();
-          try {
-            Optional<MediaFile> optionalMediaFile = mediaFileService.getMediaFile(Integer.parseInt(id));
-            if (optionalMediaFile.isPresent()) {
-              MediaFile mediaFile = optionalMediaFile.get();
-              System.out.println(YELLOW + id + ": " + mediaFile.getName() + " - " + mediaFile.getType());
-            } else {
-              System.out.println(RED + "Media file with ID " + id + " not found.");
-            }
-          } catch (NumberFormatException e) {
-            System.out.println(RED + "Invalid ID format: " + id);
-          } catch (Exception e) {
-            System.out.println(RED + "Error: " + e.getMessage());
-          }
-        }
-        case "3" -> printAllUsers(users);
+        case "1" -> printAllMediaFiles();
+        case "2" -> printMediaFileById();
+        case "3" -> printAllUsers();
+        case "4" -> printUserById();
         case "Q" -> System.out.println(RED + "Exiting the app...");
         default -> System.out.println(RED + "Invalid choice, try again!");
       }
     }
   }
 
-  private static void printAllMediaFiles(List<MediaFile> mediaFiles) {
+  private static void printAllMediaFiles() {
     System.out.println(YELLOW + "\nHere is the list of all media files:");
     for (MediaFile mediaFile : mediaFiles) {
       System.out.println(mediaFile.getId() + ": " + mediaFile.getName() + " - " + mediaFile.getType());
     }
   }
 
-  private static void printAllUsers(List<User> users) {
+  private static void printMediaFileById() {
+    {
+      System.out.print("Enter the media file ID: ");
+      String id = scanner.nextLine();
+      try {
+        Optional<MediaFile> optionalMediaFile = mediaFileService.getMediaFile(Integer.parseInt(id));
+        if (optionalMediaFile.isPresent()) {
+          MediaFile mediaFile = optionalMediaFile.get();
+          System.out.println(YELLOW + "\n" + id + ": " + mediaFile.getName() + " - " + mediaFile.getType());
+        } else {
+          System.out.println(RED + "Media file with ID " + id + " not found.");
+        }
+      } catch (NumberFormatException e) {
+        System.out.println(RED + "Invalid ID format: " + id);
+      } catch (Exception e) {
+        System.out.println(RED + "Error: " + e.getMessage());
+      }
+    }
+  }
+
+  private static void printAllUsers() {
     System.out.println(YELLOW + "\nHere is the list of all users:");
     for (org.example.entities.User user : users) {
       System.out.println(user.getId() + ": " + user.getUsername());
     }
   }
 
-  //  private List<User> getUserById(){
-//    return
-//  }
+  private static void printUserById() {
+    System.out.print("Enter the user ID: ");
+    String id = scanner.nextLine();
+    try {
+      Optional<User> optionalUser = userService.getUser(Integer.parseInt(id));
+      if (optionalUser.isPresent()) {
+        User user = optionalUser.get();
+        System.out.println(YELLOW + "\n" + id + ": " + user.getUsername());
+      } else {
+        System.out.println(RED + "User with ID " + id + " not found.");
+      }
+    } catch (NumberFormatException e) {
+      System.out.println(RED + "Invalid ID format: " + id);
+    } catch (Exception e) {
+      System.out.println("Error: " + e.getMessage());
+    }
+  }
 
 }
